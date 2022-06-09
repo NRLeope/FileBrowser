baad63c6deb3917f31b5b9a7167c72497b666dbb
TRAPSOULest1999 <neoleope9@gmail.com>
2022-06-09 17:01:19 +0200

    Added File Browsing and Opening

--- FileBrowser2.Designer.cs
+++ FileBrowser2.Designer.cs
@@ -29,31 +29,91 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileBrowser2));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDirectory = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // btnBack
             // 
-            this.pictureBox1.Image = global::FileBrowser.Properties.Resources._1081856_200;
-            this.pictureBox1.Location = new System.Drawing.Point(291, 100);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.btnBack.BackColor = System.Drawing.Color.Black;
+            this.btnBack.ForeColor = System.Drawing.Color.Transparent;
+            this.btnBack.Location = new System.Drawing.Point(12, 21);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<-";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackColor = System.Drawing.Color.Black;
+            this.btnForward.ForeColor = System.Drawing.Color.Transparent;
+            this.btnForward.Location = new System.Drawing.Point(93, 21);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.Text = "->";
+            this.btnForward.UseVisualStyleBackColor = false;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(244, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnDirectory
+            // 
+            this.btnDirectory.BackColor = System.Drawing.Color.Black;
+            this.btnDirectory.ForeColor = System.Drawing.Color.Transparent;
+            this.btnDirectory.Location = new System.Drawing.Point(680, 21);
+            this.btnDirectory.Name = "btnDirectory";
+            this.btnDirectory.Size = new System.Drawing.Size(75, 23);
+            this.btnDirectory.TabIndex = 3;
+            this.btnDirectory.Text = "Open";
+            this.btnDirectory.UseVisualStyleBackColor = false;
+            this.btnDirectory.Click += new System.EventHandler(this.btnDirectory_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(174, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Directory:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 50);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(743, 388);
+            this.webBrowser1.TabIndex = 5;
             // 
             // FileBrowser2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(765, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDirectory);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FileBrowser2";
             this.Text = "FileBrowser2";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -61,6 +121,11 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
--- FileBrowser2.cs
+++ FileBrowser2.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Forms;
+using WebBrowser = System.Windows.Forms.WebBrowser;
 
 namespace FileBrowser
 {
@@ -15,6 +17,35 @@ namespace FileBrowser
         public FileBrowser2()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDirectory_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog dialog = new FolderBrowserDialog() { Description="Select Directory:"})
+            {
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser1.Url = new Uri(dialog.SelectedPath);
+                    textBox1.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if(webBrowser1.CanGoBack)
+                webBrowser1.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+                webBrowser1.GoForward(); 
         }
     }
 }
